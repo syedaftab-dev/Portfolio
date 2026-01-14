f4f7536b104078f2895d3d7e49a3089339486886
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-01-14 14:07:12 +0530

    project section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import About from './components/About';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
           <Navbar/>
           <HeroSection/>
           <About/>
+          <Projects/>
         </main>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { PROJECTS } from '../assets/constants'
+import Card from './Card'
+
+function Projects() {
+  return (
+    <div id="projects">
+      <h2 className='mt-20 text-center text-4xl font-semibold'>Projects</h2>
+      <div className='flex flex-wrap justify-center py-8'>
+        {PROJECTS.map((project,index)=>(
+          <div key={index}>
+            <Card image={project.image} title={project.title} subtitle={project.subtitle} link={project.link}/>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Projects
