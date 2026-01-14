3bbed949c5f4759e2489bd1bb5760a6500f92f11
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-01-14 22:40:14 +0530

    refactor Projects component to use motion elements for enhanced animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,17 +2,48 @@ import React from 'react'
 import { PROJECTS } from '../assets/constants'
 import Card from './Card'
 
+import { motion } from 'framer-motion'
+
+const containerVariants = {
+  hidden: {opacity: 0,y: -20},
+  visible: {
+    opacity: 1,
+    y:0
+  },
+  transition:{
+    duration: 0.5,
+    staggerChildren: 0.4
+  }
+}
+
+const itemVariants = {
+  hidden: {opacity: 0,y: 0.8},
+  visible: {opacity : 1,scale:1 ,transition:{duration: 0.5}}
+}
+j
 function Projects() {
   return (
     <div id="projects">
-      <h2 className='mt-20 text-center text-4xl font-semibold'>Projects</h2>
-      <div className='flex flex-wrap justify-center py-8'>
+      <motion.h2 
+      initial="hidden"
+      whileInView="visible"
+      variants={containerVariants}
+      viewport={{once:true}}
+      className='mt-20 text-center text-4xl font-semibold'>Projects</motion.h2>
+      <motion.div 
+      initial="hidden"
+      whileInView="visible"
+      variants={containerVariants}
+      viewport={{once:true}}
+      className='flex flex-wrap justify-center py-8'>
         {PROJECTS.map((project,index)=>(
-          <div key={index}>
+          <motion.div 
+          variants={itemVariants}
+          key={index}>
             <Card image={project.image} title={project.title} subtitle={project.subtitle} link={project.link}/>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   )
 }
